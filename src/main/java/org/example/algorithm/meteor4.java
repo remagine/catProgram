a02ac4f6ce606e98a53b92496bff2ceae41eb576
remagine2 <remagine@naver.com>
2023-02-17 16:09:33 +0900

    0217

--- src/main/java/org/example/algorithm/meteor4.java
+++ src/main/java/org/example/algorithm/meteor4.java
@@ -21,16 +21,7 @@ public class meteor4 {
         }
         bf.close();
 
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < s; j++) {
-                System.out.print(chars[i][j]);
-            }
-            System.out.println();
-        }
-
-        // 1. 최소 충돌거리를 찾는다.
         int length = Integer.MAX_VALUE;
-
         for (int i = 0; i < r; i++) {
             int rX = 0;
             int rL = r - 1;
@@ -48,32 +39,8 @@ public class meteor4 {
                 }
             }
         }
-        length--; // 실제 움직여야할 거리
+        length--;
 
-        // 운석부분을 r + length -1
-        // 기존 운석부분은 공기 . 로 치환
-        // 기존 공기부분을 C로 치환
-        // C를 전부 X로 치환
-        /*for (int i = 0; i < r; i++) {
-            for (int j = 0; j < s; j++) {
-                char x = chars[i][j];
-                if (x == 'X') {
-                    chars[i][j] = '.';
-                    chars[i + length][j] = 'C';
-                }
-            }
-        }
-
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < s; j++) {
-                char x = chars[i][j];
-                if (x == 'C') {
-                    chars[i][j] = 'X';
-                }
-            }
-        }*/
-
-        // 운석의 밑에서부터 변환하기
         for (int i = r - 1; i >= 0; i--) {
             for (int j = 0; j < s; j++) {
                 char x = chars[i][j];
@@ -84,11 +51,13 @@ public class meteor4 {
             }
         }
 
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < s; j++) {
-                System.out.print(chars[i][j]);
+                sb.append(chars[i][j]);
             }
-            System.out.println();
+            sb.append('\n');
         }
+        System.out.print(sb);
     }
 }
